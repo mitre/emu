0d4c6fc57c0f46f4a6288883c90087114df4f04b
Anna Lin <alin@mitre.org>
2021-05-25 16:55:09 -0400

    Added GUI for Emu plugin to display abilities and adversaries

--- hook.py
+++ hook.py
@@ -3,16 +3,20 @@ import shutil
 
 from app.utility.base_world import BaseWorld
 from plugins.emu.app.emu_svc import EmuService
+from plugins.emu.app.emu_gui import EmuGUI
 
 name = 'Emu'
 description = 'The collection of abilities from the CTID Adversary Emulation Plans'
-address = None
+address = '/plugin/emu/gui'
 access = BaseWorld.Access.RED
 data_dir = os.path.join('plugins', name.lower(), 'data')
 
 
 async def enable(services):
     plugin_svc = EmuService()
+    emu_gui = EmuGUI(services, name, description)
+    app = services.get('app_svc').application
+    app.router.add_route('GET', '/plugin/emu/gui', emu_gui.splash)
 
     if not os.path.isdir(plugin_svc.repo_dir):
         await plugin_svc.clone_repo()
